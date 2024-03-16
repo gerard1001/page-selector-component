c03f2e0f1d97c57bfcac160253763d44ce2f2672
gerard1001 <rutayisiregerard2002@gmail.com>
2024-03-16 06:30:41 +0200

    Deploying in process

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pages-selector/",
+  base: "/page-selector-component/",
   plugins: [react()],
 });
